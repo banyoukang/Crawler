db753988a82a9fcc698aec14e8fe722776a94026
banyk <youkangban@163.com>
2016-09-23 15:23:01 +0800

    9/23 15:22

--- src/com/crawler/controller/Crawler01.java
+++ src/com/crawler/controller/Crawler01.java
@@ -1,8 +1,23 @@
 package com.crawler.controller;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.crawler.util.Crawlering;
+import com.crawler.util.FileOperate;
+
 public class Crawler01 {
+	private static final Crawlering crawler = new  Crawlering();
+	private static final FileOperate fileOperate = new FileOperate();
 	public static void main(String[] args) {
-		System.out.println("hello github");
-		System.out.println("第三次测试！");
+		String loginUrl = "https://www.zhihu.com/";
+		String loginHtml = crawler.getHtmlBuUrl(loginUrl);
+		Document doc = Jsoup.parse(loginHtml);
+		Elements  xx = doc.getElementsByClass("question_link");
+System.out.println(xx);
+		
+		
 	}
 }
