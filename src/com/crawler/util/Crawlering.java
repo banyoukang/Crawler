5884d2f3863c6dfca829ea452751cb5ca5abd320
banyk <youkangban@163.com>
2016-09-23 15:22:32 +0800

    9/23 15:22

--- src/com/crawler/util/Crawlering.java
+++ src/com/crawler/util/Crawlering.java
@@ -2,7 +2,9 @@ package com.crawler.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,6 +23,10 @@ public class Crawlering implements Runnable{
 	int maxUrl;
 	String searchString; 
 	
+	public Crawlering() {
+		super();
+	}
+
 	public Crawlering(String startUrl, int maxUrl, String searchString) {
 		this.startUrl = startUrl;
 		this.maxUrl = maxUrl;
@@ -50,7 +56,7 @@ public class Crawlering implements Runnable{
 	* @throws 
 	*/
 	private URL verifyUrl(String url){
-		if(!url.toLowerCase().startsWith("http://")){
+		if(!(url.toLowerCase().startsWith("http://")||url.toLowerCase().startsWith("https://"))){
 			System.out.println("<-------------非合法URL---------->");
 			return null;
 		}
@@ -144,5 +150,43 @@ public class Crawlering implements Runnable{
 		    return (url);
 		  }
 	
-	
+	public String getHtmlBuUrl(String url){
+		URL queryUrl = verifyUrl(url);
+		StringBuilder result;
+		if(queryUrl == null){
+			return null;
+		}
+		HttpURLConnection resumeConnection;
+		try {
+			StringBuffer cookie = new StringBuffer();
+			cookie.append("q_c1=6523909093114d16924b3e2f39a2e6d4|1474604722000|1474604722000; _xsrf=726a2d961d4b3e262b6ef4ec583f628d; d_c0=\"AECA5UZmlQqPTkeIPIhVp1P1Exs_HqR16LA=|1474604723\";");
+			cookie.append("_zap=262d6cc6-c39f-4968-b17e-626e88683e87; _za=dd8e8838-fd04-44da-8e5a-60bf5428e079; l_n_c=1; __utma=51854390.2070989570.1474604657.1474604657.1474607792.2;");
+			cookie.append("__utmc=51854390; __utmz=51854390.1474607792.2.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; ");
+			cookie.append("__utmv=51854390.000--|2=registration_date=20151130=1^3=entry_date=20160923=1; ");
+			cookie.append("l_cap_id=\"NWNmODJiNTFlOGY5NDM2MDhmZDA5OTE1OWY1YzBjZTE=|1474611983|d50c898dfd0b395a4caa42328cf1cd1a367c098e\"; ");
+			cookie.append("cap_id=\"NGRkYjdhNzBhMTEzNDAyMWEyOTU3ZWNiODIxYTQwOTM=|1474611983|4ca7041b0c91b00b066a4d0c0ff39214a8a1fca1\"; ");
+			cookie.append("login=\"NGVkZDY2YzVhNWI3NDgxMTk1MTJkN2ZjOGYzOThjZWU=|1474612000|c5a93de6a4353c6e97c43e6fe038a989689ff2ec\"; ");
+			cookie.append("a_t=\"2.0ABCKaFDzFQkXAAAAIFgMWAAQimhQ8xUJAECA5UZmlQoXAAAAYQJVTSBYDFgASdssqTEScyOF-hqYmtvpZZe5-tQe0YxcemDhoUsQ5Uqi7wOWSm1DHw==\"; ");
+			cookie.append("z_c0=Mi4wQUJDS2FGRHpGUWtBUUlEbFJtYVZDaGNBQUFCaEFsVk5JRmdNV0FCSjJ5eXBNUkp6STRYNkdwaWEyLWxsbDduNjFB|1474612000|91434ba539ae0f746b5539a702d70fa475c2cf4a");
+			resumeConnection = (HttpURLConnection) queryUrl.openConnection();
+			resumeConnection.setRequestProperty("Accept-Charset","utf-8");  
+	        resumeConnection.setRequestProperty("Content-Type","text/html;utf-8");  
+	        resumeConnection.setRequestProperty("Cookie",cookie.toString());  
+	     //   resumeConnection.setRequestProperty("Cookie","PortalAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+MCMuZnxpaWNwfDAwMDgyMzkwNSwwIy5mfGlpY3B8MDAwODIzOTA1LDEzMDc2NzA1NzM3NTI3MDY4NCxUcnVlLFFldU1Fa2xDelI0bEZaTTJkbVVtZGxPVmhsUVdwQWMzQlk2TCtWdlVOb1ZsRjVHZ1BMRVhMTTAwcHBKWW5WTGZLYzFPTTh2aGRydmRIVWVLR3JOb255dWpTS2lMeEhyQUlBbmtYZTVBTWlFVGpFMlF4bzRjWVRKeEhjNU5ScEhMSWJOWHdWckFTWHhuNUd5bURST0xTK2d3cUFWbThFUllPM3J1enR4aGgwT1VrTDJGMGkrUDdWcHViRm84blFrTXp4MFNyMXdtQzE3UEJkcGpGVU1nOW8xRkJoeHhzWElDdHhLVEpVSHRGMmpDNmNKS285bGJtTXZJZnlwR0k1VGpLd29TTUpaenhyb1BkQ3VOVW13Wk01T0ZEUExSK1lqajVCRitJSFc1enV0UlpXM08wWHhNaldIWk1nWHhncjF0dUc1b3E3RlRwOGhCMFVCWjAydDlGQT09LGh0dHA6Ly9vYS5zZGMuaWNiYy88L1NQPg==");  
+	        resumeConnection.connect();  
+	        InputStream urlStream = resumeConnection.getInputStream();  
+	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlStream,"utf-8"));
+	        String ss = null;  
+	        result = new StringBuilder();  
+	        while ((ss = bufferedReader.readLine()) != null) {  
+	        	result.append(ss);  
+	        }  
+	        bufferedReader.close();       
+	        resumeConnection.disconnect();  
+	        return result.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
