330222ad3d45c59e8b345ebaebecce1d3fcb1fc0
banyoukang <youkangban@163.com>
2016-09-30 14:13:23 +0800

    1

--- src/com/crawler/controller/Crawler01.java
+++ src/com/crawler/controller/Crawler01.java
@@ -1,5 +1,6 @@
 package com.crawler.controller;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -18,42 +19,31 @@ import com.crawler.util.FileOperate;
 public class Crawler01 {
 	private static final Crawlering crawler = new  Crawlering();
 	private static final FileOperate fileOperate = new FileOperate();
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String loginUrl = "https://www.zhihu.com/";
 		String loginHtml = crawler.getHtmlBuUrl(loginUrl);
 		Document doc = Jsoup.parse(loginHtml);
-//System.out.println(doc);
 		Elements  xx = doc.getElementsByClass("question_link");
 		String xx2 = xx.html();
 		String xx1 = xx.text();
- 
-		saveDataAsText("D:/parseZHIHU","/title.txt",xx1);
+		saveDataAsText("D:/parseZHIHU","/title.txt",xx2);
 		
 	}
 	
-	public static void saveDataAsText(String savePath,String fileName,String html){
-		//无法创建文件
+	public static void saveDataAsText(String savePath,String fileName,String html) throws IOException{
 		if(!StringUtils.isNotEmpty(savePath)||!StringUtils.isNotEmpty(html)){
 			return;
 		}
-		File file1 = new File(savePath);
-		if ( !file1.exists()){
-			  file1.mkdirs();
-			}
-		File file2 = new File(savePath+fileName);
-		if(!file2.exists()){
-			try {
-				file2.createNewFile();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		File file = new File(savePath+fileName);
+		 if(!file.exists()){
+			 new File(savePath).mkdirs();
+			 file.createNewFile();
+		 }
 		try {
-			PrintStream ps = new PrintStream(new FileOutputStream(file2));
-			ps.append(html);
-			ps.flush();
-			ps.close();
+			BufferedWriter  writer = new BufferedWriter(new FileWriter(file , true));
+			writer.write(html);
+			writer.flush();
+			writer.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
--- src/com/test/testFile.java
+++ src/com/test/testFile.java
@@ -1,5 +1,18 @@
 package com.test;
 
-public class testFile {
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.crawler.controller.Crawler01;
+
+public class testFile {
+	@Test
+	public void testSaveDataAsText() throws IOException{
+		Crawler01 crawler = new Crawler01();
+		crawler.saveDataAsText("D:/parseZHIHU", "/title.txt", "新字段");
+		fail("Not yet implemented");
+	}
 }
